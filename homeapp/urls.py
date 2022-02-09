64b99aa06bc7c36b90c7ee01ce08c3f48afdcd15
Raj Vishwakarma <rvrajvishwakarma099@gmail.com>
2022-02-09 19:56:26 +0530

    Uploading Taskmate Project

---
+++ homeapp/urls.py
@@ -0,0 +1,10 @@
+
+from . import views
+from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('', views.index),
+]
+
+urlpatterns += staticfiles_urlpatterns()
