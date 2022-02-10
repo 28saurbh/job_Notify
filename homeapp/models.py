6aaf5a6e2a2019e4151e04de74d45a15851b2611
Raj <rvrajvishwakarma099@gmail.com>
2022-02-10 19:36:51 +0530

    model updated

--- homeapp/models.py
+++ homeapp/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+
 class Card_Model(models.Model):
     company_name = models.CharField(max_length=50, blank=False)
-    job_title = models.CharField(max_length=50)
-    last_date = models.DateField()
-    location = models.CharField(max_length=50)
+    position = models.CharField(max_length=50, blank=True)
+    salary = models.CharField(max_length=50, blank=True, default='Not Disclosed')
+    batch = models.CharField(max_length=50, blank=True, default='Any Batch')
+    degree = models.CharField(max_length=50, blank=True , default='Not Disclosed')
+    last_date = models.CharField(max_length=50, blank=True, default='Not Disclosed')
+    location = models.CharField(max_length=50, blank=True, default='PAN India')
     apply_link = models.CharField(max_length=500)
 
     def __str__(self):
