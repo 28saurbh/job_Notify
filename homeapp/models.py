fc56766ec67b8a3727207f9a3e2c0bc6595b562e
Raj <rvrajvishwakarma099@gmail.com>
2022-02-10 19:00:42 +0530

    hhh

--- homeapp/models.py
+++ homeapp/models.py
@@ -5,7 +5,7 @@ class Card_Model(models.Model):
     job_title = models.CharField(max_length=50)
     last_date = models.DateField()
     location = models.CharField(max_length=50)
-    
+    apply_link = models.CharField(max_length=500)
 
     def __str__(self):
         return self.company_name
