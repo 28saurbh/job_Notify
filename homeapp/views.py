15a7a4ec15b5c30949a7f2fbbae14ee9889afb83
Raj <rvrajvishwakarma099@gmail.com>
2022-02-10 17:53:46 +0530

    card in index

--- homeapp/views.py
+++ homeapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Card_Model
   
 # Create your views here.
 def index(request):
       
     # render function takes argument  - request
     # and return HTML as response
-    return render(request, "index.html")
+    card = Card_Model.objects.all()
+    return render(request, "index.html", {'card' : card})
