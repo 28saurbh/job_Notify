6d0a9af2088d1bfbddf21ff06f929ce5326a780e
Raj <rvrajvishwakarma099@gmail.com>
2022-02-10 18:14:25 +0530

    ssss

--- homeapp/models.py
+++ homeapp/models.py
@@ -3,8 +3,10 @@ from django.db import models
 class Card_Model(models.Model):
     company_name = models.CharField(max_length=50, blank=False)
     job_title = models.CharField(max_length=50)
+    
     last_date = models.DateField()
     location = models.CharField(max_length=50)
+    
 
     def __str__(self):
         return self.company_name
