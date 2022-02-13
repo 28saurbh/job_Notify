bf215a6fd012271022a8205e6b3e45f540add54c
Raj <rvrajvishwakarma099@gmail.com>
2022-02-13 19:09:32 +0530

    changes made raj

--- homeapp/views.py
+++ homeapp/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 
 from .models import Card_Model, Blog_Model
-=======
-from .models import Card_Model
+
 from django.core.paginator import Paginator
 
   
 # Create your views here.
 def index(request):
-   
     # render function takes argument  - request
     # and return HTML as response
-
-    card = Card_Model.objects.all()
-    return render(request, "index.html", {'card' : card})
+    card = Card_Model.objects.all().order_by('-id')        # All Card Objects
+    paginator = Paginator(card, 4, orphans=1)          # Per Page Card
+    page_number = request.GET.get('page')           # Holds Current Page number Value
+    page_obj = paginator.get_page(page_number)          # Renders that page whose value is passed
+    return render(request, "index.html", {'card' : page_obj})
+    
 
 def blog_page(request, slug):
     blog = Blog_Model.objects.get(slug = slug)
@@ -24,8 +25,4 @@ def blog_page(request, slug):
     }
     return render(request, "blog.html", context=context)
 
-    card = Card_Model.objects.all().order_by('-id')        # All Card Objects
-    paginator = Paginator(card, 4, orphans=1)          # Per Page Card
-    page_number = request.GET.get('page')           # Holds Current Page number Value
-    page_obj = paginator.get_page(page_number)          # Renders that page whose value is passed
-    return render(request, "index.html", {'card' : page_obj})
+
