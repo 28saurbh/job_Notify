aa33066cf50cd9db73a18c67e2e0dce5cc967317
Sourabh Garewal <28saurbh@gmail.com>
2022-02-15 18:10:35 +0530

    sourabh final commit

--- homeapp/views.py
+++ homeapp/views.py
@@ -10,7 +10,7 @@ def index(request):
     # render function takes argument  - request
     # and return HTML as response
     card = Card_Model.objects.all().order_by('-id')        # All Card Objects
-    paginator = Paginator(card, 4, orphans=1)          # Per Page Card
+    paginator = Paginator(card, 10, orphans=1)          # Per Page Card
     page_number = request.GET.get('page')           # Holds Current Page number Value
     page_obj = paginator.get_page(page_number)          # Renders that page whose value is passed
     return render(request, "index.html", {'card' : page_obj})
